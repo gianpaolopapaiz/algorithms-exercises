d369702e930c5149b79fe1cce98049913b313cfb
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2022-09-21 21:10:35 -0300

    Add insertion sort solution

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,11 +15,22 @@
 
 function insertionSort(nums) {
   // code goes here
+  nums.forEach((num, i) => {
+    if (i > 0) {
+      for (let j = i; j > 0; j--) {
+        let prevNum = nums[j-1];
+        if (num < prevNum){
+          nums[j] = prevNum;
+          nums[j - 1] = num;
+        }
+      }
+    }
+  });
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
